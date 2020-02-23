96524c437fa50717748bebda2324f0fefbe48f21
Mer-ui <57984661+Mer-ui@users.noreply.github.com>
2020-02-23 16:01:56 +0100

    test

---
+++ Assets/MoneyTExt.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MoneyTExt : MonoBehaviour
+{
+    public int Moneys;
+    public Text moneytext;
+
+    // Update is called once per frame
+    void Update()
+    {
+        moneytext.text = "Money : " + Moneys;
+    }
+}
---
+++ Assets/Moralitychange.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Moralitychange : MonoBehaviour
+{
+    public float Morality;
+    public Slider moralityslider;
+
+    // Update is called once per frame
+    void Update()
+    {
+        moralityslider.value = Morality;
+    }
+
+    public void Addmorality()
+    {
+        Morality += 0.1f;
+    }
+
+    public void Removemorality()
+    {
+        Morality -= 0.1f;
+    }
+}
